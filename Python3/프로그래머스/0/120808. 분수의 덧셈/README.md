0f2272fb0d8a722ad8d0230500b6df197561a7d3
hdmswjd98 <hdmswjd98@naver.com>
2026-07-07 12:15:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/0/120808. 분수의 덧셈/README.md
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 0.01 ms
+메모리: 11.6 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 29일 14:05:14
+2026년 07월 07일 12:15:29
 
 ### 문제 설명
 
--- Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,19 +1,13 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+import math
 
-def gcd(a,b):
-    if(a<b):
-        a,b = b,a
-    while (b!=0):
-        if(a%b == 0):
-            return b
-        a,b = b, a%b
-    return 1
-
-def solution(n1,d1,n2,d2):
-
-    a,b = d1*d2, n1*d2 + n2*d1
-    g = gcd(a,b)
-    return [b//g,a//g]
-
+def solution(n1, d1, n2, d2):
+    d = d1 * d2
+    n = (n1*d2) + (n2*d1)
+    
+    g = math.gcd(d,n)
+    
+    
+    answer = [n//g, d//g]
+    
+    
+    return answer
